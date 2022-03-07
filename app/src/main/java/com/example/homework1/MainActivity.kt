ed5d512fff6d1a0da96b09bda2c1fea06a779315
hardes1 <ustinovgo@gmail.com>
2022-03-07 22:52:44 +0300

    remove tests, add viewBinding and Timber debug

--- app/src/main/java/com/example/homework1/MainActivity.kt
+++ app/src/main/java/com/example/homework1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.homework1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
