efabbe828bdd0195c16dbc384542d1f4f1b79b1d
hardes1 <ustinovgo@gmail.com>
2022-03-31 00:00:35 +0300

    search fix fragment people
    tab layout redesign
    
    streams recyclerview preview
    
    recycler streams final
    
    Content color fix
    
    status bar + app bar color change fix
    
    implemented separator line
    
    message navigation release
    
    message fragment preview
    
    streams fragment fix
    
    clean up code

--- app/src/main/java/com/example/tinkoff/recyclerFeatures/diffUtils/MessagesDiffUtil.kt
+++ app/src/main/java/com/example/tinkoff/recyclerFeatures/diffUtils/MessagesDiffUtil.kt
@@ -11,12 +11,7 @@ class MessagesDiffUtil : DiffUtil.ItemCallback<MessageContentInterface>() {
         oldItem: MessageContentInterface,
         newItem: MessageContentInterface
     ): Boolean {
-        return if (oldItem is MessageContent && newItem is MessageContent) {
-            oldItem.id == newItem.id && oldItem.type == newItem.type
-        } else if (oldItem is Date && newItem is Date) {
-            oldItem.id == newItem.id
-        } else
-            false
+        return oldItem.checkId(newItem)
     }
 
     override fun areContentsTheSame(
