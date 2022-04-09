eeb56a1a7cf7ae6357c844cb0d1421f2b9db4a69
hardes1 <ustinovgo@gmail.com>
2022-04-09 11:59:24 +0300

    profile fragment fix architecture

--- app/src/main/java/com/example/tinkoff/ui/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/tinkoff/ui/fragments/profile/ProfileFragment.kt
@@ -10,18 +10,11 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.example.tinkoff.R
 import com.example.tinkoff.data.classes.User
+import com.example.tinkoff.data.states.LoadingData
 import com.example.tinkoff.data.states.UserStatus
 import com.example.tinkoff.databinding.FragmentProfileBinding
-import com.example.tinkoff.network.Repository
-import com.google.android.material.snackbar.Snackbar
-import io.reactivex.Single
-import io.reactivex.SingleObserver
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
+import com.example.tinkoff.ui.fragments.messages.MessageFragment
 import timber.log.Timber
-import java.util.concurrent.TimeUnit
 
 
 class ProfileFragment : Fragment() {
@@ -30,7 +23,6 @@ class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding: FragmentProfileBinding
         get() = _binding!!
-    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
     private val viewModel: ProfileViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,56 +44,37 @@ class ProfileFragment : Fragment() {
         Timber.d(getString(R.string.debug_view_recreated))
         if (arguments != null) {
             val args: ProfileFragmentArgs by navArgs()
-            initializeUser(args.user, View.INVISIBLE)
-            binding.root.showNext()
+            viewModel.ownUser.value = args.user
+            viewModel.state.value = LoadingData.FINISHED
         } else {
-            if (viewModel.ownUser == null)
-                getOwnUserFromWeb()
-            else {
-                initializeUser(viewModel.ownUser, View.VISIBLE)
-                binding.root.showNext()
-            }
+            viewModel.refreshProfile(requireContext())
         }
+        viewModel.state.observe(viewLifecycleOwner) {
+            if (it != LoadingData.NONE && it.ordinal != binding.root.displayedChild)
+                binding.root.displayedChild = it.ordinal
+        }
+
+        viewModel.ownUser.observe(viewLifecycleOwner) {
+            initializeUser(
+                it,
+                when (it.id) {
+                    MessageFragment.MY_ID -> View.VISIBLE
+                    else -> View.INVISIBLE
+                }
+            )
+        }
+
     }
 
-    private fun getOwnUserFromWeb() {
-        Single.create<User> { emitter ->
-            emitter.onSuccess(Repository.generatePersonalUserData(requireContext()))
-        }.delay(DELAY_TIME, TimeUnit.MILLISECONDS).subscribeOn(Schedulers.computation())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(object : SingleObserver<User> {
 
-                override fun onSubscribe(d: Disposable) {
-                    compositeDisposable.add(d)
-                    binding.shimmerLayout.startShimmer()
-                }
-
-                override fun onSuccess(value: User) {
-                    viewModel.ownUser = value
-                    initializeUser(viewModel.ownUser, View.VISIBLE)
-                    binding.shimmerLayout.stopShimmer()
-                    binding.root.showNext()
-                }
-
-
-                override fun onError(e: Throwable) {
-                    Snackbar.make(
-                        binding.root,
-                        getString(R.string.error_profile_loading),
-                        Snackbar.LENGTH_SHORT
-                    ).show()
-                }
-            })
-    }
-
-    private fun initializeUser(user: User?, exitButtonVisibility: Int) {
-        binding.nameTextview.text = user?.name
+    private fun initializeUser(user: User, exitButtonVisibility: Int) {
+        binding.nameTextview.text = user.name
         binding.logoutButton.visibility = exitButtonVisibility
-        binding.avatarImageview.setImageResource(user?.drawableId ?: R.drawable.ic_send)
+        binding.avatarImageview.setImageResource(user.drawableId)
         binding.stateTextview.apply {
             visibility = View.VISIBLE
         }
-        when (user?.status) {
+        when (user.status) {
             UserStatus.ONLINE -> {
                 binding.onlineStatusTextview.apply {
                     text =
@@ -117,19 +90,16 @@ class ProfileFragment : Fragment() {
                     isEnabled = false
                 }
             }
-            else -> {
-                throw NotImplementedError()
-            }
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
-        compositeDisposable.dispose()
     }
 
     companion object {
+
         const val DELAY_TIME: Long = 1000
     }
 
--- app/src/main/java/com/example/tinkoff/ui/fragments/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/tinkoff/ui/fragments/profile/ProfileViewModel.kt
@@ -1,8 +1,55 @@
 package com.example.tinkoff.ui.fragments.profile
 
+import android.content.Context
+import android.view.View
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.tinkoff.R
 import com.example.tinkoff.data.classes.User
+import com.example.tinkoff.data.states.LoadingData
+import com.example.tinkoff.network.Repository
+import com.google.android.material.snackbar.Snackbar
+import io.reactivex.Single
+import io.reactivex.SingleObserver
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 class ProfileViewModel : ViewModel() {
-    var ownUser : User? = null
+    val ownUser: MutableLiveData<User> = MutableLiveData()
+    val state: MutableLiveData<LoadingData> = MutableLiveData(LoadingData.NONE)
+    private var disposable: Disposable? = null
+
+    fun refreshProfile(context: Context) {
+        disposable?.dispose()
+        if (state.value != LoadingData.FINISHED) {
+            state.value = LoadingData.LOADING
+            Single.create<User> { emitter ->
+                emitter.onSuccess(Repository.generatePersonalUserData(context))
+            }.delay(ProfileFragment.DELAY_TIME, TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : SingleObserver<User> {
+                    override fun onSubscribe(d: Disposable) {
+                        disposable = d
+                    }
+
+                    override fun onSuccess(value: User) {
+                        ownUser.value = value
+                        state.value = LoadingData.FINISHED
+                    }
+
+                    override fun onError(e: Throwable) {
+                        Timber.d(context.getString(R.string.error_profile_loading))
+                    }
+                })
+        }
+    }
+
+    override fun onCleared() {
+        disposable?.dispose()
+    }
 }
